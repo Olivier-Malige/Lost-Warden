c0a56501955f85c4128e920d0563b2aa9d6303f4
Arknoid <omalige@gmail.com>
2026-08-28 23:35:25 +0200

    fix(combat): correct shield projectile collisions

--- scenes/combat/enemy_shot.gd
+++ scenes/combat/enemy_shot.gd
@@ -17,10 +17,18 @@ func is_enemy() -> bool:
 	return true
 
 func _on_shot_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player") and (trowbackByShield or (area.has_node("shield") and area.get_node("shield").power > 0)):
-		return
-	if area.is_in_group("player") or area.is_in_group("asteroid") or (area.is_in_group("enemy") and not area.is_in_group(noDamageToGroup)):
-		if trowbackByShield:
-			area.hitByPlayerShot = true
+	if area.is_in_group("player"):
+		if trowbackByShield or (area.has_node("shield") and area.get_node("shield").power > 0):
+			return
+		_hit_area(area)
+	elif area.is_in_group("asteroid"):
+		_hit_area(area)
+	elif trowbackByShield and area.is_in_group("enemy") and not area.is_in_group(noDamageToGroup):
+		area.hitByPlayerShot = true
+		_hit_area(area)
+
+
+func _hit_area(area: Area2D) -> void:
+	if area.has_method("_hit_something"):
 		area._hit_something(damage)
-		ProjectilePool.despawn(self)
+	ProjectilePool.despawn(self)
